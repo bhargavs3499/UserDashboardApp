1e007e6624a6ae71d8a0ef79512ab1cedbe82074
Bhargav <bhargavs3499@gmail.com>
2023-11-12 11:08:26 +0530

    changes3

---
+++ config.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const EMAIL = process.env.EMAIL;
+export const PASSWORD = process.env.PASSWORD;
+export const ATLAS_URI = process.env.ATLAS_URI;
